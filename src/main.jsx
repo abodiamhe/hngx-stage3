9af40f651f637a3315318eff5134b67c047b0a78
abodiamhe <akhigbesadiq@gmail.com>
2025-02-20 11:49:59 +0100

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import env from "dotenv";
 
-createRoot(document.getElementById('root')).render(
+env.config();
+
+const detectMeta = document.createElement("meta");
+detectMeta.httpEquiv = "origin-trial";
+detectMeta.content = process.env.DETECTOR_TOKEN;
+document.head.append(detectMeta);
+
+const transMeta = document.createElement("meta");
+transMeta.httpEquiv = "origin-trial";
+transMeta.content = process.env.TRANSLATION_TOKEN;
+document.head.append(transMeta);
+
+const sumMeta = document.createElement("meta");
+sumMeta.httpEquiv = "origin-trial";
+sumMeta.content = process.env.SUMMARIZER_TOKEN;
+document.head.append(sumMeta);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
