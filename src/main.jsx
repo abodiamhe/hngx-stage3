1778b23d4bedfcc00581dd1f7da3737933ad50a2
abodiamhe <akhigbesadiq@gmail.com>
2025-02-20 12:29:23 +0100

    sent token

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import env from "dotenv";
-
-env.config();
 
 const detectMeta = document.createElement("meta");
 detectMeta.httpEquiv = "origin-trial";
-detectMeta.content = process.env.DETECTOR_TOKEN;
+detectMeta.content = import.meta.env.VITE_DETECTOR_TOKEN;
 document.head.append(detectMeta);
 
 const transMeta = document.createElement("meta");
 transMeta.httpEquiv = "origin-trial";
-transMeta.content = process.env.TRANSLATION_TOKEN;
+transMeta.content = import.meta.env.VITE_TRANSLATION_TOKEN;
 document.head.append(transMeta);
 
 const sumMeta = document.createElement("meta");
 sumMeta.httpEquiv = "origin-trial";
-sumMeta.content = process.env.SUMMARIZER_TOKEN;
+sumMeta.content = import.meta.env.VITE_SUMMARIZER_TOKEN;
 document.head.append(sumMeta);
 
 createRoot(document.getElementById("root")).render(
